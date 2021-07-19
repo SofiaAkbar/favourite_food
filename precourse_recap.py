ed415469771eadd22fe66aa329e2bb643f1c120e
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-07-19 19:05:47 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+favourite_food = input("What is your favourite food?")
+
+if favourite_food == "pizza":
+    print("I love pizza!")
+
+elif favourite_food == "broccoli":
+    print("disgusting")
+
+else:
+    print("That's a shame")
